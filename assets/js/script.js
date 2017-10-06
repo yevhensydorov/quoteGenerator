ef8bc690a047679db46e7361f188e3c0552fde84
yevhensydorov <assid@ukr.net>
2017-10-06 12:50:46 +0100

    add some flex styles v1.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-function randomColor(){
+var randomColor = function randomColorGenerator(){
 	//pick a "red" from 0-255
 	var r = Math.floor(Math.random() * 256);
 	//pick a "green" from 0-255
@@ -8,13 +8,16 @@ function randomColor(){
 	return "rgb(" + r + ", " +  g + ", " + b + ")";
 }
 
-$("body").css("background", randomColor());
+$("body").css("background", randomColor);
+var pickedColor = document.querySelector("body").style.backgroundColor;
+
+$(".new-quote").css("background", pickedColor);
 
 $(document).ready(function(){
 	$.getJSON("http://api.chrisvalleskey.com/fillerama/get.php?count=100&format=json&show=futurama", function(json){
 		var jsonData = json;
 		$(".quote-text").text(jsonData["db"][0].quote);
 		$(".quote-person").text(jsonData["db"][0].source);
-
 	});
 });
+
